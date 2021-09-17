bcaee3e640f9f9d1496328c37738d807fb2707c1
zzh <zouzhenhong@buaa.edu.cn>
2021-09-17 23:20:48 +0800

    revise code and add introduction content to README

--- README.md
+++ README.md
@@ -1,2 +1,143 @@
 # SensatUrban-BEV-Seg3D
-An efficient 3D semantic segmentation framework for Urban-scale point clouds like SensatUrban, Campus3D, etc.
+
+This is the official implementation of our BEV-Seg3D-Net, an efficient 3D semantic segmentation framework for Urban-scale point clouds like SensatUrban, Campus3D, etc. <br />
+
+Features of our framework/model:
+- leveraging various proven methods in 2D segmentation for 3D tasks
+- achieve competitive performance in the SensatUrban benchmark
+- fast inference process, about 1km^2 area per minute with RTX 3090.
+
+To be done:
+- add more complex/efficient fusion models
+- add more backbone like ResNeXt, HRNet, DenseNet, etc.
+- add more novel projection methods like pointpillars
+
+For technical details, please refer to:
+
+**Efficient Urban-scale Point Clouds Segmentation with BEV Projection** <br />
+Zhenhong Zou, Yizhe Li, Xinyu Zhang
+
+<p align='center'> <img src='imgs/bev_projection.png' width='80%'> </p>
+
+### (1) Setup
+This code has been tested with Python 3.7, PyTorch 1.8, CUDA 11.0 on Ubuntu 16.04. PyTorch of earlier versions should be supported.
+
+- Clone the repository
+```
+git clone https://github.com/zouzhenhong98/SensatUrban-BEV-Seg3D.git & cd SensatUrban-BEV-Seg3D
+```
+
+- Setup python environment
+```
+conda create -n bevseg python=3.7
+source activate bevseg
+pip install -r requirements.txt
+```
+
+### (2) Preprocess
+We provide various data analysis and preprocess methods for the SensatUrban dataset. (Part of the following steps are optional)
+
+- Before data generation, change the path_to_your_dataset in preprocess/point_EDA_31.py by:
+```
+Sensat = SensatUrbanEDA()
+Sensat.root_dir = 'path_to_your_dataset'
+Sensat.split = 'train' # change to 'test' for inference
+```
+
+- Initialize the BEV projection arguments. We provide our optimal setting below, but you can set other values for analysis:
+```
+Sensat.grids_scale = 0.05
+Sensat.grids_size = 25
+Sensat.grids_step = 25
+```
+
+- (Optional) If you want to test the sliding window points generator:
+```
+data_dir = os.path.join(self.root_dir, self.split)
+ply_list = sorted(os.listdir(data_dir))[0]
+ply_path = os.path.join(data_dir, ply_name)
+ply_data = self.load_points(ply_path, reformat=True)
+grids_data = self.grid_generator(ply_data, self.grids_size, self.grids_step, False) # return an Iterator
+```
+
+- Calculating **spatial overlap ratio** in BEV projection:
+```
+Sensat.single_ply_analysis(Sensat.exp_point_overlay_count) # randomly select one ply file
+Sensat.batch_ply_analysis(Sensat.exp_point_overlay_count) # for all ply files in the path
+```
+
+- Calculating **class overlap ratio** in BEV projection, that means we ignore overlapped points belonging to the same category:
+```
+Sensat.single_ply_analysis(Sensat.exp_class_overlay_count) # randomly select one ply file
+Sensat.batch_ply_analysis(Sensat.exp_class_overlay_count) # for all ply files in the path
+```
+
+- Test BEV projection and **3D remapping with IoU index test** (reflecting the consistency in 3D Segmentation and BEV Segmentation tasks):
+```
+Sensat.evaluate('offline', Sensat.map_offline_img2pts)
+```
+
+- BEV data generation:
+```
+Sensat.batch_ply_analysis(Sensat.exp_gen_bev_projection)
+```
+
+- Point Spatial Overlap Ratio Statistics at different projection scales
+
+<p align='center'> <img src='imgs/point_loss_distribution_scale.png' width='60%'> </p>
+<p align='center'> <img src='imgs/point_loss_ratio_scale.png' width='60%'> </p>
+
+- More BEV projection testing results refers to our sample images: completion test at imgs/completion_test, edge detection with different CV operators at imgs/edge_detection, rgb and label projection samples at imgs/projection_sample
+
+### (3) Training & Inference
+We provide two basic multimodal fusion network developped from U-Net in the modeling folder, unet.py is the basic feature fusion, and uneteca.py is the attention fusion.
+
+- Change the path_to_your_dataset in mypath.py and dataloaders/__init__.py >>> 'cityscapes'
+
+- Train from sratch
+```
+python train.py --use-balanced-weights --batch-size 8 --base-size 500 --crop-size 500 --loss-type focal --epochs 200 --eval-interval 1
+```
+
+- Change the save_dir in inference.py
+
+- Inference on test data
+```
+python inference.py --batch-size 8
+```
+
+- Prediction Results Visualization
+<p align='center'> <img src='imgs/prediction_samples.png' width='80%'> </p>
+
+### (4) Evaluation
+- Remap your BEV prediction to 3D and evaluate in 3D benchmark in preprocess/point_EDA_31.py (following the prvious initialization steps):
+```
+Sensat.evaluate_batch(Sensat.evaluate_batch_nn(Sensat.eval_offline_img2pts))
+```
+
+### (5) Citation
+If you find our work useful in your research, please consider citing: (Information is coming soon! We are asking the open-access term of the conference!)
+
+### (6) Acknowledgment
+- Part of our data processing code (read_ply and metrics) is developped based on https://github.com/QingyongHu/SensatUrban
+- Our code of neural network is developped based on a U-Net repo from the github, but unfortunately we are unable to recognize the raw github repo. Please tell us if you can help.
+
+### (7) Related Work
+To learn more about our fusion segmentation methods, please refers to our previous work:
+
+    @article{Zhang2021ChannelAI,
+        title={Channel Attention in LiDAR-camera Fusion for Lane Line Segmentation},
+        author={Xinyu Zhang and Zhiwei Li and Xin Gao and Dafeng Jin and Jun Li},
+        journal={Pattern Recognit.},
+        year={2021},
+        volume={118},
+        pages={108020}
+    }
+
+    @article{Zou2021ANM,
+        title={A novel multimodal fusion network based on a joint coding model for lane line segmentation},
+        author={Zhenhong Zou and Xinyu Zhang and Huaping Liu and Zhiwei Li and A. Hussain and Jun Li},
+        journal={ArXiv},
+        year={2021},
+        volume={abs/2103.11114}
+    }
--- preprocess/point_EDA_31.py
+++ preprocess/point_EDA_31.py
@@ -4,7 +4,7 @@
     TODO:
      - [x] multiprocessing -> ~10 Process
      - [x] complexity -> optimize
-     - [x] finally reach 100x acceleration in point clouds statistics
+     - [x] finally reach 800x acceleration in point clouds statistics
      - [x] add BEV projection function
      - [x] add class-overlay (pillar) analysis function
      - [x] add projection cover rate function
@@ -12,60 +12,18 @@
      - [x] reconstruct the code to be well-organized
      - [x] overall point clouds mIoU calculation
      - [x] seg2points mapping
-     - [ ] overlapped sampling
+     - [x] overlapped sampling
      - [x] offline mapping: map img to pts
      - [x] pixel nn completion: image, edge
      - [x] Multi-scale Sampling (grid=25,50)
      - [ ] split the code (experiment part)
-    
-    Acceleration Record:
-     - 0: Original: 05:13 at 1/400 downsampling, ETA 34.7 hours
-     - 1: stat all step time cost with cambridge_block_8 as sample
-        query time 316.8650002479 553 (99.96%) -> vital point 1
-            (in single grid)
-            query x idx 0.38154101371765137 (48.80%) -> vital point 1.1
-            query y idx 0.3851637840270996 (49.26%) -> vital point 1.2
-            query xy idx 0.015166521072387695 (1.94%)
-        normal time 0.022572755813598633
-        count time 0.14864778518676758
-        write time 0.0002963542938232422
-     - 2: split index query function -> vital point 1.1 decrease from 0.382s/grid to 0.197s/grid, totally time cost decrease to 48.34% (target: <1%)
-     - 3: merge index query function for x and y axis -> vital point 1 decrease from 0.7799s/grid to 0.195s/grid (25.06%, target: <1%)
-     - 4: move x_coor query from y-loop to x-loop -> vital point 1 decrease from 0.7799s/grid to 0.105s/grid (13.46%, target: <1%)
-     - 5: using pointer to limit the query range -> avoid searching used data -> O(N) complexity -> 2500x acceleration!
-     - 6: seperate the x-y co-sorting to be individual -> sort x first then y -> 20% faster -> 3000x acceleration!
-     - 7: revise one bug, return to be 850x speed...
-     - 8: overlay ratio count: for (0.04, 25, 25) setting
-        coordinate: around 50%
+
+     - overlay ratio count: for (0.04, 25, 25) setting
         class: over 97.7%
         miou: over 93.7%
-    - 9: details for completion
-        >>> new discovery: 
+     - details for completion
         using 0.05 may results better in completion, 
-        makinge more consisting annotation, and multi-scale referrence point matters
-        >>> key aspects in completion:
-        internal and edge consistency, empty ratio, class consistency
-        strategy -> max-pooling for internal points(, then max-item-pooling for edge)
-        >>> current plans:
-        'max + edge': max-pooling(3,1) + edge completion for all (or 'mix + edge')
-        using both 0.04/0.05; 0.05 first
-        estimate experiments: 2 x 2
-    - 10: data generation steps:
-        >>> essential data generation: rgb, alt, cla, vis (0.7s * 7680 / 2 = 45min)
-        >>> data split (data_0) -> random split on all data
-        >>> extension data generation: completed(rgb, alt, cla, vis) + canny (14s * 7680 / 2 = 15h)
-        >>> data split (data_1) -> random split on all data
-        >>> multi-scale data genertation: completed(rgb, alt, cla, vis) + canny (15h / 4 = 4h)
-        >>> data split (data_2)
-    - 11: data training steps:
-        >>> exp | model | data
-        >>> exp0 | deeplabv3+d8 | data_0
-        >>> exp1 | HRNetv2 | data_0
-        >>> exp2 | other sota | data_0 -> optimal model OM0
-        >>> exp3 | OM0 | data_1 SM
-        >>> exp4 | OM0 | data_1 MM -> optimal data pair OD1
-        >>> exp5 | OM0 | data_2+OD1 MM -> optimal data pair OD2
-
+        makinge more consisting annotation, and multi-scale referrence point
 """
 
 from typing import Iterator, Tuple
@@ -316,71 +274,6 @@ class SensatUrbanEDA(object):
 
         return bev, xs, ys, num
 
-    # To be deprecated
-    # refers to helper_image.altitude_dif_func_v1(v2)
-    # 
-    # def altitude_difference(self, alt_map, border):
-    #     alt_df = alt_map.copy()
-    #     idx_valid = np.where(alt_map>-1000) # (?,N)
-    #     for ii in range(len(idx_valid[0])):
-    #         x = idx_valid[0][ii]
-    #         y = idx_valid[1][ii]
-    #         if x>1 and x<border-1:
-    #             if y>1 and y<border-1:
-    #                 idx_lst = [(x+2,y),(x-2,y),(x,y+2),(x,y-2)]
-    #                 idx_able = []
-    #                 for idx in idx_lst:
-    #                     if idx[0] in idx_valid[0] and idx[1] in idx_valid[1]:
-    #                         idx_able.append(idx)
-    #                 df = [alt_map[x, y]]
-    #                 for idx in idx_able:
-    #                     z = alt_map[x, y]
-    #                     zN = alt_map[idx[0], idx[1]]
-    #                     df.append(abs(z-zN))
-    #                 alt_df[x, y] = np.mean(df)
-    #     return alt_df
-    
-    # To be deprecated
-    # refers to helper_image.project_3d_bev_img
-    # 
-    # def project_3d_bev_img(self, grid_data: np.array, grid_scale: float, grid_size: float, imgid: int):
-    #     """project 3d points to 2d bev pixels (BEV Projection)"""
-    #     grid_size_scale = int(grid_size / grid_scale) # size of projection
-    #     num = grid_data.shape[1]
-    #     # project elements
-    #     bev = np.zeros((grid_size_scale, grid_size_scale, 4)) # (grid_scale, grid_scale, 1)
-    #     bev[:, :, 0] -= 1000 # ensure smaller than z
-    #     vis = np.zeros((grid_size_scale, grid_size_scale, 3))
-    #     cls = np.zeros((grid_size_scale, grid_size_scale))
-    # 
-    #     xs = (grid_data[0] / grid_scale).astype(np.int32)
-    #     ys = (grid_data[1] / grid_scale).astype(np.int32)
-    #     for i in range(num):
-    #         if bev[xs[i], ys[i], 0] < grid_data[2, i]:
-    #             # update points
-    #             bev[xs[i], ys[i], 0] = grid_data[2, i]
-    #             # update RGB:
-    #             bev[xs[i], ys[i], 3] = grid_data[3, i] # R
-    #             bev[xs[i], ys[i], 2] = grid_data[4, i] # G
-    #             bev[xs[i], ys[i], 1] = grid_data[5, i] # B
-    #             # update class (label)
-    #             cls[xs[i], ys[i]] = grid_data[6, i]
-    #             # update class_vis (label in color)
-    #             vis[xs[i], ys[i], 2] = self.label_color_map[int(cls[xs[i], ys[i]])][0] # R
-    #             vis[xs[i], ys[i], 1] = self.label_color_map[int(cls[xs[i], ys[i]])][1] # G
-    #             vis[xs[i], ys[i], 0] = self.label_color_map[int(cls[xs[i], ys[i]])][2] # B
-    #     alt = bev[:, :, 0] # altitude(z)
-    #     alt_df = self.altitude_difference(alt, grid_size_scale-1)
-    #     rgb = bev[:, :, 1:]
-    # 
-    #     store = 'gen_bev/gen_{}_{}_{}'.format(self.grids_scale, self.grids_size, self.grids_step)
-    #     if not os.path.exists(store): os.mkdir(store)
-    #     cv2.imwrite(store+'/{}_alt.png'.format(imgid), alt)
-    #     cv2.imwrite(store+'/{}_adf.png'.format(imgid), alt_df)
-    #     cv2.imwrite(store+'/{}_img.png'.format(imgid), rgb)
-    #     cv2.imwrite(store+'/{}_cls.png'.format(imgid), cls)
-    #     cv2.imwrite(store+'/{}_vis.png'.format(imgid), vis)
-
     # ===========================================================================
     #
     #                               Experiment tools
@@ -405,54 +298,9 @@ class SensatUrbanEDA(object):
             content.append((x_idx, y_idx, num, OAcc))
         return content
 
-    def exp_gen_bev_projection_v0(self, grids_data: Iterator[Tuple[int, int, np.array]], ply_name) -> list:
-        """generate BEV projection for altitude(z), RGB, class, class_vis"""
-        for grid in grids_data:
-            x_idx, y_idx, pts = grid
-            # self.project_3d_bev_img(pts.T, self.grids_scale, self.grids_step, x_idx+'_'+y_idx)
-            imgid = ply_name[:-4]+'_'+str(x_idx)+'_'+str(y_idx) # city_block_id_x_y_type.png
-            alt, rgb, cla, vis = hi.project_3d_bev_img4(pts.T, self.grids_scale, self.grids_step, self.label_color_map, imgid)
-            store = hi.write_root0
-            cv2.imwrite(store+'/alt/{}_alt.png'.format(imgid), alt)
-            cv2.imwrite(store+'/rgb/{}_rgb.png'.format(imgid), rgb)
-            cv2.imwrite(store+'/cls/{}_cls.png'.format(imgid), cla)
-            cv2.imwrite(store+'/vis/{}_vis.png'.format(imgid), vis)
-        return None
-
-    def exp_gen_bev_projection_v1(self, grids_data: Iterator[Tuple[int, int, np.array]], ply_name) -> list:
+    def exp_gen_bev_projection(self, grids_data: Iterator[Tuple[int, int, np.array]], ply_name) -> list:
         """generate BEV projection for altitude(z), alt_canny, RGB, class, class_vis
-        compared with v0: 1) conduct completion; 2) add canny for altitude
-        """
-        for grid in grids_data:
-            x_idx, y_idx, pts = grid
-            # self.project_3d_bev_img(pts.T, self.grids_scale, self.grids_step, x_idx+'_'+y_idx)
-            imgid = ply_name[:-4]+'_'+str(x_idx)+'_'+str(y_idx) # city_block_id_x_y_type.png
-            alt, rgb, cla, vis = hi.project_3d_bev_img4(pts.T, self.grids_scale, self.grids_step, self.label_color_map, imgid)
-            
-            # completion
-            n_loop = 3
-            alt = hi.complete2d(alt, n_loop)
-            cla = hi.complete2d(cla, n_loop)
-            for c in range(3):
-                vis[:, :, c] = hi.complete2d(vis[:, :, c], n_loop) # to be optimize by colorize cla-comp
-                rgb[:, :, c] = hi.complete2d(rgb[:, :, c], n_loop)
-            
-            # edge analysis
-            alt_cp = alt.copy()
-            alt_cp[alt==-1000] = 0
-            alt_canny = hi.canny2d(alt_cp.astype(np.uint8), blur=False, lb=2, hb=5)
-            
-            store = hi.write_root1
-            cv2.imwrite(store+'/alt/{}_alt.png'.format(imgid), alt)
-            cv2.imwrite(store+'/canny/{}_alt.png'.format(imgid), alt_canny)
-            cv2.imwrite(store+'/rgb/{}_rgb.png'.format(imgid), rgb)
-            cv2.imwrite(store+'/cls/{}_cls.png'.format(imgid), cla)
-            cv2.imwrite(store+'/vis/{}_vis.png'.format(imgid), vis)
-        return None
-
-    def exp_gen_bev_projection_v11(self, grids_data: Iterator[Tuple[int, int, np.array]], ply_name) -> list:
-        """generate BEV projection for altitude(z), alt_canny, RGB, class, class_vis
-        compared with v1: 1) add invalid point as unlabeled annotation
+        Notice: we add invalid point as unlabeled annotation
         """
         for grid in grids_data:
             x_idx, y_idx, pts = grid
@@ -795,12 +643,4 @@ if __name__=='__main__':
     # Sensat.single_ply_analysis(Sensat.exp_class_overlay_count) # single file demo
     # Sensat.batch_ply_analysis()
 
-    # Sensat.batch_ply_analysis(Sensat.exp_gen_bev_projection_v0) # generate data_0
-    # Sensat.batch_ply_analysis(Sensat.exp_gen_bev_projection_v1) # generate data_1
-    # Sensat.batch_ply_analysis(Sensat.exp_gen_bev_projection_v11) # generate data_11
-
-    # generate data_2
-    # Sensat.grids_scale = 0.1
-    # Sensat.grids_size = 50
-    # Sensat.grids_step = 50
-    # Sensat.batch_ply_analysis(Sensat.exp_gen_bev_projection_v11)
+    # Sensat.batch_ply_analysis(Sensat.exp_gen_bev_projection) # generate data
